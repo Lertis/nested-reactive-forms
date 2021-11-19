e7f8c0db67e8c58c94d6861d4003f2cf07616e8d
Yura Husak <yura.husak@juriba.com>
2021-11-19 11:01:20 +0200

    Added highcharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,85 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "angular-highcharts";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	chart: Chart;
 
+	ngOnInit() {
+		this.init();
+	}
+
+	addPoint() {
+		if (this.chart) {
+			this.chart.addPoint(Math.floor(Math.random() * 10));
+		} else {
+			alert('init chart, first!');
+		}
+	}
+
+	addSerie() {
+		this.chart.addSeries({
+			name: 'Line ' + Math.floor(Math.random() * 10),
+			type: "line",
+			data: [
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10)
+			],
+		}, true, true);
+	}
+
+	removePoint() {
+		this.chart.removePoint(this.chart.ref.series[0].data.length - 1);
+	}
+
+	removeSerie() {
+		this.chart.removeSeries(this.chart.ref.series.length - 1);
+	}
+
+	init() {
+		const chart = new Chart({
+			chart: {
+				type: 'line',
+				zoomType: 'x',
+				events: {
+					redraw: function () {
+						console.log('x0');
+					},
+					selection: function (event) {
+						console.log(event.xAxis);
+						console.log(event.yAxis);
+						return true;
+					},
+				},
+			},
+			title: {
+				text: 'Linechart'
+			},
+			credits: {
+				enabled: false
+			},
+			series: [
+				{
+					name: 'Line 1',
+					type: "line",
+					data: [1, 2, 3]
+				},
+			],
+		});
+		chart.addPoint(4);
+		this.chart = chart;
+		chart.addPoint(5);
+		setTimeout(() => chart.addPoint(6), 20);
+		chart.ref$.subscribe(console.log);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { ChartModule } from "angular-highcharts";
+
 import { AppComponent } from "./app.component";
-import { ProfileComponent } from "./profile/profile.component";
-import { CreditComponent } from "./profile/info/credit/credit.component";
-import { AddressComponent } from "./profile/info/address/address.component";
-import { InfoComponent } from "./profile/info/info.component";
 import { CheckboxComponent } from "./profile/checkbox/checkbox.component";
+import { AddressComponent } from "./profile/info/address/address.component";
+import { CreditComponent } from "./profile/info/credit/credit.component";
+import { InfoComponent } from "./profile/info/info.component";
+import { ProfileComponent } from "./profile/profile.component";
 import { ProjectsTasksComponent } from "./profile/projects-tasks/projects-tasks.component";
 import { SimpleFieldComponent } from "./shared/simple.input";
 
 @NgModule({
-	imports: [CommonModule, BrowserModule, ReactiveFormsModule, FormsModule],
+	imports: [CommonModule, BrowserModule, ReactiveFormsModule, FormsModule, ChartModule],
 	declarations: [
 		AppComponent,
 		ProfileComponent,
